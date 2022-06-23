a44a0560e814906ea740cd31558185e26a13b5e1
Precious <preciouschinazaekpere200113>
2022-06-23 08:38:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// On my console screen
+let myName = 'Okafor Precious Chinazaekpere';
+let myHeight = '5.8inches';
+let myCountry = 'Nigeria';
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry);
+
